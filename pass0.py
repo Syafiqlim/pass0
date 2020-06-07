3ee6eb828a48da73e5814044f793c05a9ca340ef
Syafiqlim <38598679+Syafiqlim@users.noreply.github.com>
2020-06-07 23:38:34 +0800

    Add files via upload

---
+++ pass0.py
@@ -0,0 +1,17 @@
+import time
+
+print("  ____ ___  _   _  ____ ____      _  _____ ____  _ ")
+print(" / ___/ _ \| \ | |/ ___|  _ \    / \|_   _/ ___|| |")
+print("| |  | | | |  \| | |  _| |_) |  / _ \ | | \___ \| |")
+print("| |__| |_| | |\  | |_| |  _ <  / ___ \| |  ___) |_|")
+print(" \____\___/|_| \_|\____|_| \_\/_/   \_\_| |____/(_)")
+time.sleep(1)
+
+print("\nCONGRATULATIONS! YOU WILL GET THE PASSWORD!")
+print("BTW, email me your Instagram username on syafiqmuhammad588@gmail.com, I will follow you.")
+time.sleep(2)
+
+print("\nDecrypt the WiFi password :")
+print("code : WTB1QXIzUjBjSzV0QHIh")
+print("enctype : base64")
+print("\nHehe, ENJOY!")
